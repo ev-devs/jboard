53ab17d387f3d9cf34084c54619c461092a61472
jruel006 <jruel006@ucr.edu>
2016-07-24 16:13:42 -0700

    the modals work and are coming together well

--- jboard.js
+++ jboard.js
@@ -1,3 +1,55 @@
-$('.modal-trigger').leanModal({
+/* $('.keyboard-trigger').leanModal({
     opacity: 0, // Opacity of modal background
+});
+
+$('.keyboard-trigger').leanModal({
+    opacity: 0, // Opacity of modal background
+});
+*/
+
+
+
+
+
+/**LOGIC FOR THE KEYBOARD*/
+
+var jboard = function(arg, input_field) {
+
+
+    if (arg == "standard") {
+        setupStandardEventHandlers(input_field)
+    }
+    else if (arg == "num") {
+        setupNumEventHandlers(input_field)
+    }
+    else {
+        console.log('ERROR: Invalid Argument to jboard')
+    }
+
+}
+
+function setupStandardEventHandlers(input_field){
+
+    input_field.click(function(){
+        $('#keyboard-modal').openModal({
+            opacity: 0, // Opacity of modal background
+        });
+    });
+
+
+}
+
+function setupNumEventHandlers(input_field){
+    input_field.click(function(){
+        $('#num-modal').openModal({
+            opacity: 0, // Opacity of modal background
+        });
+    });
+}
+
+jQuery.fn.extend({
+    jboard : function(arg) {
+        return new jboard(arg, this) // instantiate a new jboard
+    }
+
 });
