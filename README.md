4983d280ce833da864fd89fa6a00278bf97cd339
jruel006 <jruel006@ucr.edu>
2016-07-25 10:51:10 -0700

    adding exportation html for the keyboard

---
+++ README.md
@@ -0,0 +1,7 @@
+# jBoard
+A virtual keyboard and numpad written on top of jQuery and Materialize.css
+
+## Installation
+	`$ git clone https://github.com/ev-devs/jboard.git`
+	`$ cd jboard`
+	
--- jboard.js
+++ jboard.js
@@ -1,29 +1,3 @@
-/*THIS IS FOR ERROR OUTPUTTING*/
-colors = {
-    reset: '\033[0m',
-
-    //text color
-
-    black: '\033[30m',
-    red: '\033[31m',
-    green: '\033[32m',
-    yellow: '\033[33m',
-    blue: '\033[34m',
-    magenta: '\033[35m',
-    cyan: '\033[36m',
-    white: '\033[37m',
-
-    //background color
-
-    blackBg: '\033[40m',
-    redBg: '\033[41m',
-    greenBg: '\033[42m',
-    yellowBg: '\033[43m',
-    blueBg: '\033[44m',
-    magentaBg: '\033[45m',
-    cyanBg: '\033[46m',
-    whiteBg: '\033[47m'
-}
 //var clickSound = new Audio('bin/buttonclick.mp3');
 var clickSound = document.getElementById("click-sound")
 
